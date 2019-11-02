899c28ef3cf9d914452aed45e1c269c3b1509349
xiangqingyi <1352493573@qq.com>
2019-11-02 14:50:29 +0800

    [UPDATE] 更新主页面文件

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,72 @@
 <template>
-    <div id="app" class="fillcontain">
+    <div id="app" class="container-fluid wrap">
         <router-view></router-view>
     </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+   export default {
+     name: 'app'
+   }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+   #app {
+     font-family: 'Consolas', Helvetica, Arial, sans-serif;
+     -webkit-font-smoothing: antialiased;
+     -moz-osx-font-smoothing: rayscale;
+     }
+  .cartImg {
+    width: 80px;
+    height: 80px;
+  }
+
+  a{
+    color: #333;
+  }
+  a:hover{
+    color: #e4393c;
+    text-decoration: none
+  }
+  .focus, .verify-error {
+    border-color: #d01a39;
+    outline: 0;
+    -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.75), 0 0 8px rgba(208, 26, 57, 0.6);
+    box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.75), 0 0 8px rgba(208, 26, 57, 0.6);
+  }
+  .verify-error-msg {
+    color: #d01a39;
+    display: block;
+    margin-top: 10px;
+    min-height: 20px;
+    line-height: 20px;
+    background: url("../static/images/tips.png") no-repeat left -1px;
+    padding-left: 25px;
+    margin-bottom: 0;
+    clear: both
+  }
+  .wrap {
+    padding: 0
+  }
+  ::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 10px;
+    background-color: #F5F5F5;
+  }
+  ::-webkit-scrollbar {
+    width: 8px;
+    background-color: #F5F5F5;
+  }
+  ::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    background-color: #888;
+  }
+  a.link {
+    color: #e4393c;
+  }
+  a.link:hover {
+    color: #b32b2e;
+    text-decoration: none;
+  }
 </style>
